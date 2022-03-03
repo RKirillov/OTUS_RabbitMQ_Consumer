7640d6188bc835c30a1c4d306ec23c7d0a35ec6b
1 <1>
2022-03-03 20:55:55 +0300

    Initial commit

---
+++ Consumers/Consumer.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer.Consumers
+{
+    public static class Consumer
+    {
+        public static void Register(IConnection connectionInfo, string name, int number)
+        {
+            string queueName = $"queue.{name}_{number}";
+            string routingKey = $"cars.{number}";
+            /*
+            string routingKey = $"cars.*";
+            if (number > 2)
+            {
+                routingKey = $"bicycles.{number}";
+            }
+            */
+            using (var connection = connectionInfo)
+            using (var channel = connection.CreateModel())
+            {
+                channel.BasicQos(0, 10, false);
+
+                channel.QueueDeclare(queueName, false, false, false, null);
+                channel.QueueBind(queueName, $"exchange.{name}", routingKey, null);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (sender, e) =>
+                {
+                    Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ff")} Received message");
+
+                    Thread.Sleep(TimeSpan.FromSeconds(2));
+                    var body = e.Body;
+                    var message = JsonSerializer.Deserialize<MessageDto>(Encoding.UTF8.GetString(body.ToArray()));
+                    Console.WriteLine("  Received message: {0}", message.Content);
+                    channel.BasicAck(e.DeliveryTag, false);
+                };
+
+                channel.BasicConsume(queue: queueName,
+                    autoAck: false,
+                    consumer: consumer);
+
+                Console.WriteLine("Subscribed to the queue");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Consumers.Consumer.Register(GetRabbitConnection(), "direct", int.Parse(args[0]));
+            //Consumers.Consumer.Register(GetRabbitConnection(), "fanout", int.Parse(args[0]));
+            Consumers.Consumer.Register(GetRabbitConnection(), "topic", int.Parse(args[0]));
+            //Consumers.Consumer.Register(GetRabbitConnection(), "direct.instant", int.Parse(args[0]));
+
+        }
+
+        static private IConnection GetRabbitConnection()
+        {
+            ConnectionFactory factory = new ConnectionFactory
+            {
+                UserName = "ykziztbb",
+                Password = "oZaUpy2Sru1P0b04K9ghjx3MSFpXTMIU",
+                VirtualHost = "ykziztbb",
+                HostName = "hawk.rmq.cloudamqp.com"
+            };
+            IConnection conn = factory.CreateConnection();
+            return conn;
+        }
+    }
+}
